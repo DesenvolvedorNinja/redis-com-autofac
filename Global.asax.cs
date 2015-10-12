8efa32e0d821f2657831a9f2f482008e75d8df5b
Romulo Cianci <rcianci@gmail.com>
2015-10-11 22:49:38 -0300

    Código-fonte do exemplo publicado no blog http://desenvolvedor.ninja

---
+++ Global.asax.cs
@@ -0,0 +1,61 @@
+﻿using Autofac;
+using Autofac.Integration.Wcf;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace AutofacDemoWCF
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your service implementations.
+            builder.RegisterType<AutofacDemoWCF.Service1>();
+            builder.RegisterType<Soma>().As<ICalculo>().InstancePerLifetimeScope();
+
+            builder.Register<ConnectionMultiplexer>(c => ConnectionMultiplexer.Connect("pub-redis-13919.us-east-1-2.5.ec2.garantiadata.com:13919,password=salamandraverde")).SingleInstance();
+            builder.Register<IDatabase>(c => c.Resolve<ConnectionMultiplexer>().GetDatabase());
+
+            // Set the dependency resolver.
+            AutofacHostFactory.Container = builder.Build();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace AutofacDemoWCF
+{
+    public class Service1 : IService1
+    {
+        private ICalculo _calculo;
+        private IDatabase _database;
+
+        public Service1(ICalculo calculo, IDatabase database)
+        {
+            this._calculo = calculo;
+            this._database = database;
+        }
+
+        public string GetTeste() => "Desenvolvedor Ninja - Autofac com WCF";
+
+        public double GetSoma(double a, double b) => _calculo.Calcular(a, b);
+
+        public bool SetRedis(string key, string value) => _database.StringSet(key, value);
+
+        public string GetRedis(string key) => _database.StringGet(key);
+    }
+}
